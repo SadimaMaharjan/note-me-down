4b111f8394d2955f47f006267a9cd93a44db74b9
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-05-09 17:43:04 +1000

    adds push functionality

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 // import Express.js
 const express = require("express");
 
-const { v4: uuidv4 } = require("uuid");
+var uniqid = require("uniqid");
 
-//import file system
 const fs = require("fs");
 
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require("path");
-
-// Import database
-const noteData = require("./db/db.json");
 
 // Initialize an instance of Express.js
 const app = express();
@@ -19,9 +15,9 @@ const app = express();
 const PORT = 3001;
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Static middleware pointing to the public folder
 app.use(express.static("public"));
 
@@ -37,7 +33,7 @@ app.get("/notes", (req, res) => {
 // GET /api/notes should read the db.json file and return all the saved data.
 
 app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", (error, data) => {
+  fs.readFile("db/db.json", (error, data) => {
     if (error) throw error;
     res.json(JSON.parse(data));
   });
@@ -45,7 +41,29 @@ app.get("/api/notes", (req, res) => {
 
 // POST api/notes should receive a new note to save and add it to db.json file
 app.post("/api/notes", (req, res) => {
-  const { title, text } = req.body;
+  let db = fs.readFileSync("db/db.json");
+  db = JSON.parse(db);
+  res.json(db);
+  // creating body for note
+  let userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    // creating unique id for each note
+    id: uniqid(),
+  };
+  // pushing created note to be written in the db.json file
+  db.push(userNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
+  res.json(db);
+});
+
+// DELETE should receive a query parameter that contains the id of a note to delete
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("db/db.json", (error, data) => {
+    if (error) throw error;
+    let notes = JSON.parse(data);
+  });
 });
 
 // listen() method is responsible for listening for incoming connections on the specified port
